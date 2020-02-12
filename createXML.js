30551bd28e7d1f2c30bcc6000fbcf60bbbfcf52a
Ilya Bykov <il.byckoff2011@hotmail.com>
2020-02-13 02:15:38 +0300

    Add script for creating xml files.

--- createXML.js
+++ createXML.js
@@ -1,6 +1,44 @@
 const fs = require('fs');
 const path = require('path');
 
-const inputPath = path.resolve(__dirname, './output-data/output.txt');
+const {
+    getParagraphsFromText,
+    getSentencesFromText
+} = require('./helpers/nlpHelper');
 
-let dataBuffer = fs.readFileSync(inputPath);
+const {
+    createXmlHeader,
+    createXmlParagraph,
+    createXmlSentence,
+    createXmlFile
+} = require('./helpers/xmlHelper');
+
+const inputFilePath = path.resolve(__dirname, './output-data/output.txt');
+const outputXmlPath = path.resolve(__dirname, './output-data/document.xml');
+
+fs.readFile(inputFilePath, (err, dataBuffer) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    const data = dataBuffer.toString();
+
+    let xmlRoot = createXmlHeader();
+
+    const paragraphs = getParagraphsFromText(data);
+    paragraphs.forEach(paragraph => {
+        if (paragraph) {
+            let paragraphBlock = createXmlParagraph(xmlRoot);
+
+            const sentences = getSentencesFromText(paragraph);
+            sentences.forEach(sentence => {
+                if (sentence)
+                    createXmlSentence(paragraphBlock, sentence);
+            });
+        }
+    });
+
+    let xml = xmlRoot.end({ pretty: true });
+    createXmlFile(outputXmlPath, xml);
+});
